ddc2b89e44aad21f1f788553c0ce97b8ef935836
pawan4476 <nagam.tiger@gmail.com>
2017-02-23 16:33:19 +0530

    Initial commit

--- PushNotificationDemo/AppDelegate.h
+++ PushNotificationDemo/AppDelegate.h
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Pushbots/Pushbots.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) Pushbots *pushbotsClient;
 
 @end
 
--- PushNotificationDemo/AppDelegate.m
+++ PushNotificationDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Pushbots/Pushbots.h>
 
 @interface AppDelegate ()
 
@@ -17,9 +18,51 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.pushbotsClient = [[Pushbots alloc] initWithAppId:@"58abf1a84a9efa3ffc8b4567" prompt:YES];
+    [self.pushbotsClient trackPushNotificationOpenedWithLaunchOptions:launchOptions];
+    NSDictionary *userInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (userInfo) {
+        
+        NSDictionary *aps = [userInfo objectForKey:@"aps"];
+        if (aps) {
+            NSString *alertMsg = [aps objectForKey:@"alert"];
+               NSLog(@"Notification message: %@", alertMsg);
+        }
+        NSString *articleID = [userInfo objectForKey:@"articleId"];
+    }
     return YES;
 }
 
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    [Pushbots openURL:userInfo];
+    if (application.applicationState == UIApplicationStateInactive) {
+        [self.pushbotsClient trackPushNotificationOpenedWithPayload:userInfo];
+    }
+    
+    if (application.applicationState == UIApplicationStateActive) {
+        NSDictionary *notificationDict = [userInfo objectForKey:@"aps"];
+        NSString *alertString = [notificationDict objectForKey:@"alert"];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Push Notification Received" message:alertString preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+        [alert addAction:cancel];
+        [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
+        
+    }
+    
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    [self.pushbotsClient registerOnPushbots:deviceToken];
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"notification register error %@", [error description]);
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
